03109cf00d35da47220fd0b8ee48a52337968617
ilhamtubagus <ilhamta27@gmail.com>
2021-10-05 13:18:44 +0700

    initial commit

---
+++ public/electron.js
@@ -0,0 +1,64 @@
+const path = require("path");
+
+const { app, BrowserWindow, ipcMain } = require("electron");
+const isDev = require("electron-is-dev");
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    maxWidth: 1000,
+    maxHeight: 680,
+    maximizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+      webSecurity: false,
+    },
+    fullscreenable: false,
+    minWidth: 1000, // set a min width!
+    minHeight: 680, // and a min height!
+  });
+
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  win.on("maximize", () => {
+    win.unmaximize();
+  });
+  // Open the DevTools.
+  if (isDev) {
+    win.webContents.openDevTools({ mode: "detach" });
+  }
+  if (!isDev) {
+    win.removeMenu();
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+ipcMain.handle("getAppPath", function (evt, arg) {
+  // Send message back to renderer.
+  return app.getAppPath();
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,452 @@
-import logo from './logo.svg';
-import './App.css';
+import path, { join, resolve } from "path";
 
+import "./App.css";
+import {
+  Typography,
+  FormControl,
+  Select,
+  MenuItem,
+  Grid,
+  Paper,
+  Divider,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Slider,
+  Button,
+} from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
+import React from "react";
+import { HexColorPicker } from "react-colorful";
+import { useDebounce } from "use-debounce";
+import Backdrop from "@mui/material/Backdrop";
+import Snackbar from "@mui/material/Snackbar";
+import CircularProgress from "@mui/material/CircularProgress";
+const ipcRenderer = window.require("electron").ipcRenderer;
+const exec = window.require("child_process").exec;
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 function App() {
+  const [effect, setEffect] = React.useState("static");
+  const [colorCount, setColorCount] = React.useState(0);
+  const [brightness, setBrightness] = React.useState(0);
+  const [speed, setSpeed] = React.useState(0);
+  const [snackbar, setSnackbar] = React.useState({
+    msg: "",
+    open: false,
+    severity: "info",
+  });
+  const [color1, setColor1] = React.useState("#363537");
+  const [color2, setColor2] = React.useState("#363537");
+  const [color3, setColor3] = React.useState("#363537");
+  const [color4, setColor4] = React.useState("#363537");
+  //debounce colors for performance
+  const [color1D] = useDebounce(color1, 200);
+  const [color2D] = useDebounce(color2, 200);
+  const [color3D] = useDebounce(color3, 200);
+  const [color4D] = useDebounce(color4, 200);
+  let rogAuraExecPath = "";
+  const [selectedColor, setSelectedColor] = React.useState(0);
+  const handleChangeEffect = (event, newValue) => {
+    setEffect(newValue);
+  };
+  const handleColorCountChange = (event) => {
+    if (event.target.value === 0) {
+      setSelectedColor(0);
+    }
+    if (event.target.value === 1) {
+      setEffect("static");
+    }
+    setColorCount(event.target.value);
+  };
+  const handleChangeBrightness = (event) => {
+    setBrightness(event.target.value);
+  };
+  const handleChangeSpeed = (event) => {
+    setSpeed(event.target.value);
+  };
+  const handleOpenSnackbar = (msg, severity) => {
+    setSnackbar({ msg: msg, open: true, severity: severity });
+  };
+  const handleCloseSnackbar = () => {
+    setSnackbar({
+      msg: "",
+      open: false,
+      severity: "info",
+    });
+  };
+
+  const handleChangePicker = (newColor) => {
+    switch (selectedColor) {
+      case 0:
+        setColor1(newColor);
+        break;
+      case 1:
+        setColor2(newColor);
+        break;
+      case 2:
+        setColor3(newColor);
+        break;
+      case 3:
+        setColor4(newColor);
+        break;
+      default:
+    }
+  };
+  const [openBackdrop, setOpenBackdrop] = React.useState(false);
+  const handleClose = () => {
+    setOpenBackdrop(false);
+  };
+  const handleToggleBackdrop = () => {
+    setOpenBackdrop(!openBackdrop);
+  };
+  // apply setting
+  const handleApplySettings = async () => {
+    handleToggleBackdrop();
+    // apply setting by calling rogauracore bin file
+    if (rogAuraExecPath === "") {
+      let appPath = await ipcRenderer.invoke("getAppPath");
+      if (path.basename(appPath) === "app.asar") {
+        appPath = path.dirname(appPath);
+        appPath = appPath.replace(/ /g, "\\ ");
+      }
+      rogAuraExecPath = join(appPath + "/bin/rogauracore");
+    }
+    let execCmd = ``;
+    // single section
+    if (colorCount === 0) {
+      switch (effect) {
+        case "static":
+          execCmd = `${rogAuraExecPath} single_static ${color1D.substring(1)}`;
+          break;
+        case "breathing":
+          execCmd = `${rogAuraExecPath} single_breathing ${color1D.substring(
+            1
+          )} ${color2D.substring(1)} ${speed}`;
+
+          break;
+        case "cycle":
+          execCmd = `${rogAuraExecPath} single_colorcycle ${speed}`;
+          break;
+        default:
+          break;
+      }
+    } else {
+      // multiple section
+      switch (effect) {
+        case "static":
+          execCmd = `${rogAuraExecPath} multi_static ${color1D.substring(
+            1
+          )} ${color2D.substring(1)} ${color3D.substring(
+            1
+          )} ${color4D.substring(1)}`;
+          break;
+        case "breathing":
+          execCmd = `${rogAuraExecPath} multi_breathing ${color1D.substring(
+            1
+          )} ${color2D.substring(1)} ${color3D.substring(
+            1
+          )} ${color4D.substring(1)} ${speed}`;
+
+          break;
+        default:
+          break;
+      }
+    }
+    console.log(
+      `pkexec sh -c "${rogAuraExecPath}  initialize_keyboard; echo '${brightness}' > /sys/class/leds/asus::kbd_backlight/brightness; ${execCmd}; systemctl restart upower.service"`
+    );
+    exec(
+      `pkexec sh -c "${rogAuraExecPath}  initialize_keyboard; echo '${brightness}' > /sys/class/leds/asus::kbd_backlight/brightness; ${execCmd}; systemctl restart upower.service"`,
+      (err, stdout, stderr) => {
+        handleClose();
+        if (err) {
+          handleOpenSnackbar(
+            "Oops snap!.. we couldn't apply ur settings",
+            "error"
+          );
+        }
+      }
+    );
+  };
+
+  const renderColorSelection = () => {
+    if (colorCount === 0) {
+      if (effect === "static") {
+        return (
+          <React.Fragment>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                padding: 5,
+              }}
+            >
+              <div
+                onClick={() => setSelectedColor(0)}
+                style={{
+                  width: 45,
+                  height: 45,
+                  backgroundColor: color1D,
+                  borderRadius: 2,
+                  cursor: "pointer",
+                  border:
+                    selectedColor === 0
+                      ? "5px solid #46a1d9"
+                      : "5px solid #eceef3",
+                }}
+              />
+            </div>
+          </React.Fragment>
+        );
+      } else if (effect === "breathing") {
+        return (
+          <React.Fragment>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                padding: 5,
+              }}
+            >
+              <div
+                onClick={() => setSelectedColor(0)}
+                style={{
+                  width: 45,
+                  height: 45,
+                  backgroundColor: color1D,
+                  borderRadius: 2,
+                  cursor: "pointer",
+                  border:
+                    selectedColor === 0
+                      ? "5px solid #46a1d9"
+                      : "5px solid #eceef3",
+                  marginRight: "30px",
+                }}
+              />
+              <div
+                onClick={() => setSelectedColor(1)}
+                style={{
+                  width: 45,
+                  height: 45,
+                  backgroundColor: color2D,
+                  borderRadius: 2,
+                  cursor: "pointer",
+                  border:
+                    selectedColor === 1
+                      ? "5px solid #46a1d9"
+                      : "5px solid #eceef3",
+                }}
+              />
+            </div>
+          </React.Fragment>
+        );
+      }
+    } else {
+      return (
+        <React.Fragment>
+          {" "}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              padding: 5,
+            }}
+          >
+            <div
+              onClick={() => setSelectedColor(0)}
+              style={{
+                width: 45,
+                height: 45,
+                backgroundColor: color1D,
+                borderRadius: 2,
+                cursor: "pointer",
+                border:
+                  selectedColor === 0
+                    ? "5px solid #46a1d9"
+                    : "5px solid #eceef3",
+                marginRight: "30px",
+              }}
+            />
+
+            <div
+              onClick={() => setSelectedColor(1)}
+              style={{
+                width: 45,
+                height: 45,
+                backgroundColor: color2D,
+                borderRadius: 2,
+                cursor: "pointer",
+                border:
+                  selectedColor === 1
+                    ? "5px solid #46a1d9"
+                    : "5px solid #eceef3",
+                marginRight: "30px",
+              }}
+            />
+            <div
+              onClick={() => setSelectedColor(2)}
+              style={{
+                width: 45,
+                height: 45,
+                backgroundColor: color3D,
+                borderRadius: 2,
+                cursor: "pointer",
+                border:
+                  selectedColor === 2
+                    ? "5px solid #46a1d9"
+                    : "5px solid #eceef3",
+                marginRight: "30px",
+              }}
+            />
+            <div
+              onClick={() => setSelectedColor(3)}
+              style={{
+                width: 45,
+                height: 45,
+                backgroundColor: color4D,
+                borderRadius: 2,
+                cursor: "pointer",
+                border:
+                  selectedColor === 3
+                    ? "5px solid #46a1d9"
+                    : "5px solid #eceef3",
+              }}
+            />
+          </div>
+        </React.Fragment>
+      );
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Paper
+      sx={{
+        bgcolor: "#ebeef3",
+        display: "flex",
+        height: "575px",
+        padding: 5,
+      }}
+    >
+      <Snackbar
+        open={snackbar.open}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+      >
+        <Alert
+          onClose={handleCloseSnackbar}
+          severity={snackbar.severity}
+          sx={{ width: "100%" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {snackbar.msg}
+        </Alert>
+      </Snackbar>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={openBackdrop}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Grid container spacing={5}>
+        <Grid item sm={7}>
+          <Typography sx={{ marginLeft: 1 }}>COLOR</Typography>
+          <HexColorPicker
+            style={{ width: "100%" }}
+            onChange={handleChangePicker}
+          />
+        </Grid>
+        <Grid item sm={5}>
+          <Paper sx={{ padding: 3 }}>
+            <Typography sx={{ marginLeft: 1 }}>MODE</Typography>
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+              <Select
+                value={colorCount}
+                onChange={handleColorCountChange}
+                displayEmpty
+                inputProps={{ "aria-label": "Without label" }}
+                fullWidth
+              >
+                <MenuItem value={0}>Single Section</MenuItem>
+                <MenuItem value={1}>Multiple Section</MenuItem>
+              </Select>
+            </FormControl>
+            {renderColorSelection()}
+            <Divider sx={{ marginTop: 1 }} />
+            <Typography sx={{ marginLeft: 1, marginTop: 2 }}>EFFECT</Typography>
+            <FormControl component="fieldset" sx={{ marginLeft: 1 }}>
+              <RadioGroup
+                aria-label="gender"
+                name="controlled-radio-buttons-group"
+                value={effect}
+                onChange={handleChangeEffect}
+              >
+                <FormControlLabel
+                  value="static"
+                  control={<Radio />}
+                  label="Static"
+                />
+                <FormControlLabel
+                  value="breathing"
+                  control={<Radio />}
+                  label="Breathing"
+                />
+                {colorCount === 0 && (
+                  <FormControlLabel
+                    value="cycle"
+                    control={<Radio />}
+                    label="Color Cycle"
+                  />
+                )}
+              </RadioGroup>
+            </FormControl>
+            <Divider />
+            <Typography sx={{ marginLeft: 1, marginTop: 2, marginBottom: 2 }}>
+              BRIGHTNESS
+            </Typography>
+            <Slider
+              aria-label="Always visible"
+              defaultValue={50}
+              onChange={handleChangeBrightness}
+              valueLabelDisplay="auto"
+              value={brightness}
+              step={1}
+              min={0}
+              max={4}
+              marks
+              sx={{ marginBottom: 2 }}
+            />
+            {effect !== "static" && (
+              <React.Fragment>
+                <Divider />
+                <Typography
+                  sx={{ marginLeft: 1, marginTop: 2, marginBottom: 2 }}
+                >
+                  SPEED
+                </Typography>
+                <Slider
+                  aria-label="Always visible"
+                  defaultValue={50}
+                  onChange={handleChangeSpeed}
+                  valueLabelDisplay="auto"
+                  value={speed}
+                  step={1}
+                  min={1}
+                  max={3}
+                  marks
+                  sx={{ marginBottom: 2 }}
+                />
+              </React.Fragment>
+            )}
+            <Button variant="contained" onClick={handleApplySettings}>
+              Apply
+            </Button>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 }
 
